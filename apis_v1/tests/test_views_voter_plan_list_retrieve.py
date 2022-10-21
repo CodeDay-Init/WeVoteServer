e9e8634d866c7f478acf506f1dd4f01122707f06
Chris <rolletchris1@gmail.com>
2022-10-21 15:48:38 -0700

    added valid/unvalid voter_id response

--- apis_v1/tests/test_views_voter_plan_list_retrieve.py
+++ apis_v1/tests/test_views_voter_plan_list_retrieve.py
@@ -16,10 +16,30 @@ class WeVoteAPIsV1TestsVoterPlanListRetrieve(TestCase):
         self.voter_plan_list_retrieve_url = reverse("apis_v1:voterPlanListRetrieveView")
 
     def test_retrieve_with_no_voter_device_id(self):
+        """Without a cookie, we don't expect valid response"""
         response = self.client.get(self.voter_plan_list_retrieve_url)
         json_data = json.loads(response.content.decode())
 
+        # Without a cookie, we don't expect valid response
+        self.assertEqual('status' in json_data, True, "status expected in the json response, and not found")
+        self.assertEqual('voter_device_id' in json_data, True,
+                         "voter_device_id expected in the voterPlanListRetrieveView json response, and not found")
+
+        self.assertEqual(
+            json_data['status'], 'VALID_VOTER_DEVICE_ID_MISSING ',  # Space needed
+            "status: {status} (VALID_VOTER_DEVICE_ID_MISSING expected), "
+            "voter_device_id: {voter_device_id}".format(
+                status=json_data['status'], voter_device_id=json_data['voter_device_id']))
+
     def test_retrieve_with_voter_device_id(self):
+        """Test the various cookie states :return:"""
+        # Generate the voter_plan_list_retrieve_url cookie
         response = self.client.get(self.voter_plan_list_retrieve_url)
         json_data = json.loads(response.content.decode())
+
+        # Make sure we got back a voter_device_id we can use
+        self.assertEqual('voter_device_id' in json_data, True,
+                         "voter_device_id expected in the voterPlanListRetrieveView json response")
+
+        # Now put the voter_device_id in a variable we can use below
         voter_device_id = json_data['voter_device_id'] if 'voter_device_id' in json_data else ''
